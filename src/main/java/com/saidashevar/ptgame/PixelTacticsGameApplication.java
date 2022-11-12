0e897f86f8c933c7c70f61db8262e60772931325
Artur <specialforhomework@gmail.com>
2022-11-12 15:49:30 +0300

    Initial commit

---
+++ src/main/java/com/saidashevar/ptgame/PixelTacticsGameApplication.java
@@ -0,0 +1,13 @@
+package com.saidashevar.ptgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelTacticsGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelTacticsGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saidashevar/ptgame/PixelTacticsGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidashevar.ptgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelTacticsGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
