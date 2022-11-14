c3a7f8ab823e3c8c3c55233b0b51a1d34260eeae
Artur <specialforhomework@gmail.com>
2022-11-14 15:42:52 +0300

    gameId now in uri. You can see it on the game page!

--- src/main/resources/static/js/socket_js.js
+++ src/main/resources/static/js/socket_js.js
@@ -1,6 +1,10 @@
 window.onload = getID;
 
 function getID() {
+	const params = new Proxy(new URLSearchParams(window.location.search), {
+  	get: (searchParams, prop) => searchParams.get(prop),
+	});
+	let gameId = params.gameid;
 	let paragraph = document.getElementById("ShowId");
 	paragraph.textContent += gameId;	
 }
