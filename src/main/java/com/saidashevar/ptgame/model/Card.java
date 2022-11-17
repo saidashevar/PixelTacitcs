2f20b9ca6f52041b94cfb047d5730e2a91c9a06d
Artur <specialforhomework@gmail.com>
2022-11-17 22:30:18 +0300

    Added cards into deck on server

--- src/main/java/com/saidashevar/ptgame/model/Card.java
+++ src/main/java/com/saidashevar/ptgame/model/Card.java
@@ -1,11 +1,13 @@
 package com.saidashevar.ptgame.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Card {
 	
 	private String name;
-	private Integer attack;
-	private Integer health;
+	private int attack;
+	private int health;
 }
--- src/main/java/com/saidashevar/ptgame/model/Game.java
+++ src/main/java/com/saidashevar/ptgame/model/Game.java
@@ -1,6 +1,7 @@
 package com.saidashevar.ptgame.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import lombok.Data;
 
@@ -13,10 +14,66 @@ public class Game {
 	private Player player2;
 	private ArrayList<Card> discardPilePlayer1 = new ArrayList<>(10);
 	private ArrayList<Card> discardPilePlayer2 = new ArrayList<>(10);
-	private ArrayList<Card> deckPlayer1 = new ArrayList<>(21);
-	private ArrayList<Card> deckPlayer2 = new ArrayList<>(21);
+	private ArrayList<Card> deckPlayer1 = new ArrayList<>(25);
+	private ArrayList<Card> deckPlayer2 = new ArrayList<>(25);
 	private ArrayList<Card> handPlayer1 = new ArrayList<>(6);
 	private ArrayList<Card> handPlayer2 = new ArrayList<>(6);
 	private String[][] boardPlayer1 = {{"","",""},{"","",""},{"","",""}};
-	private String[][] boardPlayer2 = {{"","",""},{"","",""},{"","",""}}; 
+	private String[][] boardPlayer2 = {{"","",""},{"","",""},{"","",""}};
+	
+	{
+		deckPlayer1.add(new Card("Cursed Knight", 3, 6));
+		deckPlayer1.add(new Card("Diabolist", 1, 3));
+		deckPlayer1.add(new Card("Divinity", 2, 4));
+		deckPlayer1.add(new Card("Druid", 2, 5));
+		deckPlayer1.add(new Card("Chronicler", 1, 4));
+		deckPlayer1.add(new Card("Immortal", 3, 2));
+		deckPlayer1.add(new Card("Inventor", 1, 5));
+		deckPlayer1.add(new Card("Lorekeeper", 2, 4));
+		deckPlayer1.add(new Card("Magical Knight", 2, 4));
+		deckPlayer1.add(new Card("Puppeter", 2, 4));
+		deckPlayer1.add(new Card("Relic Hunter", 3, 7));
+		deckPlayer1.add(new Card("Sage", 0, 4));
+		deckPlayer1.add(new Card("Plague Bearer", 3, 4));
+		deckPlayer1.add(new Card("Monster Hunter", 3, 5));
+		deckPlayer1.add(new Card("Mastermind", 3, 6));
+		deckPlayer1.add(new Card("Planebinder", 2, 5));
+		deckPlayer1.add(new Card("Operative", 2, 5));
+		deckPlayer1.add(new Card("Necromancer", 2, 6));
+		deckPlayer1.add(new Card("Warmage", 4, 3));
+		deckPlayer1.add(new Card("Tactitian", 0, 4));
+		deckPlayer1.add(new Card("Supervillain", 0, 3));
+		deckPlayer1.add(new Card("Warlock", 2, 3));
+		deckPlayer1.add(new Card("Technologist", 3, 3));
+		deckPlayer1.add(new Card("Sniper", 6, 3));
+		deckPlayer1.add(new Card("Werewolf", 3, 5));
+		Collections.shuffle(deckPlayer1);
+		
+		deckPlayer2.add(new Card("Cursed Knight", 3, 6));
+		deckPlayer2.add(new Card("Diabolist", 1, 3));
+		deckPlayer2.add(new Card("Divinity", 2, 4));
+		deckPlayer2.add(new Card("Druid", 2, 5));
+		deckPlayer2.add(new Card("Chronicler", 1, 4));
+		deckPlayer2.add(new Card("Immortal", 3, 2));
+		deckPlayer2.add(new Card("Inventor", 1, 5));
+		deckPlayer2.add(new Card("Lorekeeper", 2, 4));
+		deckPlayer2.add(new Card("Magical Knight", 2, 4));
+		deckPlayer2.add(new Card("Puppeter", 2, 4));
+		deckPlayer2.add(new Card("Relic Hunter", 3, 7));
+		deckPlayer2.add(new Card("Sage", 0, 4));
+		deckPlayer2.add(new Card("Plague Bearer", 3, 4));
+		deckPlayer2.add(new Card("Monster Hunter", 3, 5));
+		deckPlayer2.add(new Card("Mastermind", 3, 6));
+		deckPlayer2.add(new Card("Planebinder", 2, 5));
+		deckPlayer2.add(new Card("Operative", 2, 5));
+		deckPlayer2.add(new Card("Necromancer", 2, 6));
+		deckPlayer2.add(new Card("Warmage", 4, 3));
+		deckPlayer2.add(new Card("Tactitian", 0, 4));
+		deckPlayer2.add(new Card("Supervillain", 0, 3));
+		deckPlayer2.add(new Card("Warlock", 2, 3));
+		deckPlayer2.add(new Card("Technologist", 3, 3));
+		deckPlayer2.add(new Card("Sniper", 6, 3));
+		deckPlayer2.add(new Card("Werewolf", 3, 5));
+		Collections.shuffle(deckPlayer2);
+	}
 }
