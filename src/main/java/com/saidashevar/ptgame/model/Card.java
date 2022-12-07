a5eb42893bacc7b66da298fbfdd2c950c003b3ba
Artur Saidashev <specialforhomework@gmail.com>
2022-12-08 02:24:32 +0300

    Ready to deploy hibernate. Everything is fine.

--- src/main/java/com/saidashevar/ptgame/model/Card.java
+++ src/main/java/com/saidashevar/ptgame/model/Card.java
@@ -1,8 +1,5 @@
 package com.saidashevar.ptgame.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +7,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "cards")
 public class Card {
 	
 	private String name;
